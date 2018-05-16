5051c2042fe81c783bbdc76c27e588be257b7c55
github username <github email address>
2018-05-16 09:28:05 -0400

    initial setup/first commit

--- db/migrate/20180515211051_create_dreams.rb
+++ db/migrate/20180515211051_create_dreams.rb
@@ -1,8 +1,8 @@
 class CreateDreams < ActiveRecord::Migration[5.2]
   def change
     create_table :dreams do |t|
-      t.string :content,
-      t.string :adjectives,
+      t.string :content
+      t.string :adjectives
       t.string :collage
 
       t.timestamps
